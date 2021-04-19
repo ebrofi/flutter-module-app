2be87eb384223d0fb7593a3498cd1984c3ca3e16
Nic Schittenhelm <ebrofi@gmail.com>
2021-04-19 12:34:21 +0200

    Readme update

---
+++ README.md
@@ -0,0 +1,7 @@
+# kurs_app_2021
+
+A new Flutter application.
+
+## Getting Started
+
+Test Aoo
---
+++ android/app/src/main/kotlin/de/kurs_app_2021/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.kurs_app_2021
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
